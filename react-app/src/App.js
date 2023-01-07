754df7e528b34f4700202605a44f212583907526
koomekevo <koome.kevo@gurucrafts.net>
2023-01-07 18:26:32 +0300

    Use useReducer() hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
+      <input type="checkbox" value={checked} onChange={setChecked} />
       <label>{checked ? "checked" : "not checked"}</label>
     </div>
   );
