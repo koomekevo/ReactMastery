bfa6506c377ea2f22f9ea5a363ed7e8da2f26200
koomekevo <koome.kevo@gurucrafts.net>
2023-01-07 12:48:37 +0300

    Add useEffect() hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
 
+  useEffect(() => {
+    console.log(`The mood is ${emotion}"`);
+  },[emotion]);
+
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>Change Emotion</button>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
