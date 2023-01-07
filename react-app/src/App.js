b9becec01c47b7b93ef8a915dd4fe1957eb010f1
koomekevo <koome.kevo@gurucrafts.net>
2023-01-07 16:22:02 +0300

    Demo more uses of dependency array of useEffect()

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,16 +3,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
-    console.log(`The mood is ${emotion}"`);
-  },[emotion]);
+    console.log(`Current mood is ${emotion}"`);
+  },[emotion, secondary]);
+
+  useEffect(() => {
+    console.log(`Current secondary mood is ${secondary}"`);
+  },[secondary]);
 
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+      <h2>Current secondary emotion is {secondary}.</h2>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
